d459b4e1ad32e0706176b8fe2d592c22b2319c80
Yeniel Landestoy <yeniel.landestoy.mindapps@gmail.com>
2023-03-07 23:58:57 +0100

    initial commit

---
+++ CabifyMobileChallenge/CabifyMobileChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabifyMobileChallengeApp.swift
+//  CabifyMobileChallenge
+//
+//  Created by Yeniel Landestoy on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CabifyMobileChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
